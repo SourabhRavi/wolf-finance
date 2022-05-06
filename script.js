169215bf777dd8f9a6bd5d0f63d25fafb7dc694e
SourabhRavi <skravi0657@gmail.com>
2022-05-06 23:19:46 +0530

    u

--- script.js
+++ script.js
@@ -27,3 +27,75 @@
 //     });
 
 // }).scroll();
+
+// hero section height equal to window height
+$(window).on('load', function () {
+    var windowHeight = $(window).height();
+    $('#hero').css('height', windowHeight);
+}).trigger('resize');
+
+function setTranslate(yPos, el) {
+    el.style.backgroundPosition = `0px ${yPos - 100}px`;
+}
+
+var aboutImg = document.querySelector("#about-img");
+var tradingImg = document.querySelector("#trading-img");
+var ctaImg = document.querySelector("#cta");
+
+var xScrollPosition;
+var yScrollPosition;
+
+function scrollLoop() {
+    xScrollPosition = window.scrollX;
+    yScrollPosition = window.scrollY;
+    // console.log("scroll:", xScrollPosition, yScrollPosition)
+
+    setTranslate((yScrollPosition * 0.2) - 300, aboutImg);
+    setTranslate((yScrollPosition * 0.2) - 800, tradingImg);
+    setTranslate((yScrollPosition * 0.2) - 1000, ctaImg);
+
+    window.requestAnimationFrame(scrollLoop);
+}
+scrollLoop();
+
+
+document.addEventListener('mousemove', (e) => {
+    if (e.clientX > window.innerWidth - 20) {
+        document.querySelector('html').classList.add('active-scrollbar');
+    }
+    else {
+        document.querySelector('html').classList.remove('active-scrollbar');
+    }
+});
+
+gsap.set("#circle", { xPercent: -20, yPercent: -20 });
+
+const ball = document.querySelector("#circle");
+const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+const mouse = { x: pos.x, y: pos.y };
+const speed = 0.35;
+
+const xSet = gsap.quickSetter(ball, "x", "px");
+const ySet = gsap.quickSetter(ball, "y", "px");
+
+window.addEventListener("mousemove", e => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
+gsap.ticker.add(() => {
+
+    // adjust speed for higher refresh monitors
+    const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
+
+    pos.x += (mouse.x - pos.x) * dt;
+    pos.y += (mouse.y - pos.y) * dt;
+    xSet(pos.x);
+    ySet(pos.y);
+});
+
+//momentum scroll   
+$(window).on('load', function () {
+    var windowHeight = $(window).height();
+    $('#hero').css('height', windowHeight);
+}).trigger('resize');
